8cbfbda24e58123d5e1e25999576f0ac7484f102
Gimas-aza <101817133+Gimas-aza@users.noreply.github.com>
2022-03-26 15:11:56 +0300

    V1.0

---
+++ js/script.js
@@ -0,0 +1,44 @@
+function testWebP(callback) {
+    var webP = new Image();
+    webP.onload = webP.onerror = function () {
+        callback(webP.height == 2);
+    };
+    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+    
+testWebP(function (support) {
+    if (support == true) {
+        document.querySelector('body').classList.add('webp');
+    }else{
+        document.querySelector('body').classList.add('no-webp');
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() { // Аналог $(document).ready(function(){
+    let select = function () {
+        let selectHeader = document.querySelectorAll('.select__header');
+        let selectItem = document.querySelectorAll('.select__item');
+
+        selectHeader.forEach(item => {
+            item.addEventListener('click', selectToggle)
+        });
+
+        selectItem.forEach(item => {
+            item.addEventListener('click', selectChoose)
+        });
+
+        function selectToggle() {
+            this.parentElement.classList.toggle('is-active');
+        }
+
+        function selectChoose() {
+            let text = this.innerText,
+                select = this.closest('.select'),
+                currentText = select.querySelector('.select__current');
+            currentText.innerText = text;
+            select.classList.remove('is-active');
+
+        }
+    };
+    select();
+})
